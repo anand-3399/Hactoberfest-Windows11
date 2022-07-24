5d1a2b631143964f4d5f19eaf5a1f11d05d115be
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-24 23:25:40 +0530

    Added Animations
    Co-Authored-By: Anand Jaiswar <anandjaiswar02@gmail.com>

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 Menu = document.getElementById("Menu")
 startmenu = document.getElementsByClassName("startmenu")[0]
 
-Menu.addEventListener("click",()=>{
-    if(startmenu.style.bottom == "50px"){
+Menu.addEventListener("click", () => {
+    if (startmenu.style.bottom == "50px") {
         startmenu.style.bottom = "-655px"
     }
-    else{
+    else {
         startmenu.style.bottom = "50px"
         searchmenu.style.bottom = "-655px"
+        startmenu.style.transform = "translateX(0)"
+
     }
 })
 
 SearchIcon = document.getElementById("SearchIcon")
 searchmenu = document.getElementsByClassName("searchmenu")[0]
 
-SearchIcon.addEventListener("click",()=>{
-    if(searchmenu.style.bottom == "50px"){
+SearchIcon.addEventListener("click", () => {
+    if (searchmenu.style.bottom == "50px") {
         searchmenu.style.bottom = "-655px"
+
     }
-    else{
+    else {
         searchmenu.style.bottom = "50px"
         startmenu.style.bottom = "-655px"
     }
@@ -32,39 +35,51 @@ SearchIcon.addEventListener("click",()=>{
 Number.prototype.pad = function (n) {
     for (var r = this.toString(); r.length < n; r = 0 + r);
     return r;
-  };
-  
-  function updateClock() {
+};
+
+function updateClock() {
     var now = new Date();
-    var milli = now.getMilliseconds()%10,
-      sec = now.getSeconds(),
-      min = now.getMinutes(),
-      hou = now.getHours(),
-      mo = now.getMonth(),
-      dy = now.getDate(),
-      yr = now.getFullYear();
+    var milli = now.getMilliseconds() % 10,
+        sec = now.getSeconds(),
+        min = now.getMinutes(),
+        hou = now.getHours(),
+        mo = now.getMonth(),
+        dy = now.getDate(),
+        yr = now.getFullYear();
     var months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
     ];
     var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
-      corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
+        corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
     for (var i = 0; i < tags.length; i++)
-      document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
-  }
-  
-    updateClock();
-    window.setInterval("updateClock()", 1);
-  
-  // END CLOCK SCRIPT
+        document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+}
+
+updateClock();
+window.setInterval("updateClock()", 1);
+
+// END CLOCK SCRIPT
 // End
+
+clickanywhere = document.getElementById("myVideo")
+clickanywhere.addEventListener("click", () => {
+    if (startmenu.style.bottom == "50px") {
+        startmenu.style.bottom = "-655px"
+    }
+    else if (searchmenu.style.bottom == "50px") {
+        searchmenu.style.bottom = "-655px"
+
+    }
+
+})
