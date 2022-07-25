d4cfd802186cb963c7404a27e3620e916a02d6bf
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-25 23:57:42 +0530

    Updated Search Bar
    Co-Authored-By: Anand Jaiswar <anandjaiswar02@gmail.com>

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ Number.prototype.pad = function (n) {
 
 function updateClock() {
     var now = new Date();
-    var milli = now.getMilliseconds() % 10,
-        sec = now.getSeconds(),
+    var sec = now.getSeconds(),
         min = now.getMinutes(),
         hou = now.getHours(),
         mo = now.getMonth(),
         dy = now.getDate(),
-        yr = now.getFullYear();
+        yr = now.getFullYear(),
+        ampm = "AM";
     var months = [
         "January",
         "February",
@@ -61,8 +61,18 @@ function updateClock() {
         "November",
         "December"
     ];
+
+    if (hou >= 12) {
+        ampm = "PM";
+        if (hou > 12)
+            hou = hou - 12;
+    }
+    else if (hou == 0) {
+        hou = 12;
+        ampm = "AM";
+    }
     var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
-        corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
+        corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), ampm];
     for (var i = 0; i < tags.length; i++)
         document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
 }
